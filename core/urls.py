98b18187e1705a9381150645cb97931091d9aafb
Rhett Sean Flanagan <rhettseanflanagan@gmail.com>
2025-08-10 14:50:59 +0200

    CSS Tables for Styling

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/add/', views.CustomerCreateView.as_view(), name='customer_add'),
+    path('customers/<int:pk>/edit/', views.CustomerUpdateView.as_view(), name='customer_edit'),
+    path('customers/<int:pk>/delete/', views.CustomerDeleteView.as_view(), name='customer_delete'),
+
+    path('transactions/', views.transaction_list, name='transaction_list'),
+    path('transactions/add/', views.TransactionCreateView.as_view(), name='transaction_add'),
+    path('transactions/<int:pk>/edit/', views.TransactionUpdateView.as_view(), name='transaction_edit'),
+    path('transactions/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name='transaction_delete'),
+]
